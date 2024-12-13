b21771dd9ac4db48befdc45c18bbc86727663526
Matt Bengston <bengsfort@gmail.com>
2024-12-14 00:23:41 +0200

    Add transform to actor

--- src/core/scene/actor.h
+++ src/core/scene/actor.h
@@ -1,34 +1,27 @@
 #pragma once
 
-<<<<<<< HEAD
 #include <vector>
 #include <memory>
 
 #include "scene/scene.h"
 #include "scene/component.h"
-=======
 #include "scene/transform.h"
->>>>>>> 5b30f9166fc12b9bc8f08d4579dcdeb4d10e8658
 
 namespace sauna_scene
 {
 
 class Actor
 {
+public:
+    Transform transform;
+
 private:
     Scene* m_scene;
     std::vector<std::unique_ptr<Component>> m_components;
 
 public:
-<<<<<<< HEAD
     Actor(Scene* scene);
     virtual ~Actor();
-=======
-
-public:
-    Actor() {}
-    virtual ~Actor() = default;
->>>>>>> 5b30f9166fc12b9bc8f08d4579dcdeb4d10e8658
 
     template<typename T, typename... Args>
     T* addComponent(Args&&... args);
